0420df830e96f585a059602d3ab73ebc8ed9ae01
layth morrar <layth.morrar@gmail.com>
2024-01-04 01:35:06 +0400

    add LMY.MSWordEditorManual

---
+++ LMY.MSWordEditorManual/LMYMSWordEditorOptions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+
+namespace LMY.MSWordEditor
+{
+    public class LMYMSWordEditorOptions
+    {
+        public Func<string, HttpContext, bool> OnAuthentication { get; set; }
+        public Func<string, string, HttpContext, byte[]> OnGetFile { get; set; }
+        public Func<string, string, byte[], HttpContext, bool> OnSaveFile { get; set; }
+        public Action<string, HttpContext> OnError { get; set; }
+    }
+}
